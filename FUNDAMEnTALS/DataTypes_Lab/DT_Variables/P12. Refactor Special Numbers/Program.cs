254be3a54842e6122558cafe0119d25f22743004
sndicheva <100013953+sndicheva@users.noreply.github.com>
2022-05-28 21:45:23 +0300

    Added problem 12

--- FUNDAMEnTALS/DataTypes_Lab/DT_Variables/P12. Refactor Special Numbers/Program.cs
+++ FUNDAMEnTALS/DataTypes_Lab/DT_Variables/P12. Refactor Special Numbers/Program.cs
@@ -6,7 +6,24 @@ namespace P12._Refactor_Special_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int input = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int currentNum = 0;
+            bool isSpecial = false;
+            for (int i = 1; i <= input; i++)
+            {
+                currentNum = i;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+                isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", currentNum, isSpecial);
+                sum = 0;
+                i = currentNum;
+            }
+
         }
     }
 }

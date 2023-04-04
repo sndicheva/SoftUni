f59248e1feb238cc675b27579a3cdd9ab5b8a9ea
sndicheva <100013953+sndicheva@users.noreply.github.com>
2023-04-04 19:41:15 +0300

    Add files via upload

---
+++ FUNDAMEnTALS/FundRetake_2023/01_SecretChat/Program.cs
@@ -0,0 +1,35 @@
+﻿string message = Console.ReadLine();
+
+string command = Console.ReadLine();
+
+while (command != "Reveal")
+{
+    string[] tokens = command.Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+
+    if (tokens[0] == "InsertSpace")
+    {
+        message = message.Insert(int.Parse(tokens[1]), " ");
+        Console.WriteLine(message);
+    }
+    else if (tokens[0] == "Reverse")
+    {
+        if (message.Contains(tokens[1]))
+        {
+            message = message.Remove(message.IndexOf(tokens[1]), tokens[1].Length);
+            message += new string(tokens[1].Reverse().ToArray());
+            Console.WriteLine(message);
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+    }
+    else if (tokens[0] == "ChangeAll")
+    {
+        message = message.Replace(tokens[1], tokens[2]);
+        Console.WriteLine(message);
+    }
+    
+    command = Console.ReadLine();
+}
+Console.WriteLine($"You have a new text message: {message}");

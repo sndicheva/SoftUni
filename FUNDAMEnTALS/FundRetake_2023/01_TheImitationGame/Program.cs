e12c9f17c15b1846f363578e530bf7b00baba1d5
sndicheva <100013953+sndicheva@users.noreply.github.com>
2023-04-04 16:21:07 +0300

    Add files via upload

---
+++ FUNDAMEnTALS/FundRetake_2023/01_TheImitationGame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+
+string message = Console.ReadLine();
+
+string command = Console.ReadLine();
+
+while (command != "Decode")
+{
+    string[] tokens = command.Split("|", StringSplitOptions.RemoveEmptyEntries);
+
+    string action = tokens[0];
+    if (action == "Move")
+    {
+        int countOfLettersToMove = int.Parse(tokens[1]);
+
+        string substring = message.Substring(0, countOfLettersToMove);
+        message = message.Remove(0, countOfLettersToMove) + substring;
+    }
+    else if (action == "Insert")
+    {
+        int index = int.Parse(tokens[1]);
+        string value = tokens[2];
+
+        message = message.Insert(index, value);
+    }
+    else // change all
+    {
+        string substring = tokens[1];
+        string replacement = tokens[2];
+
+        message = message.Replace(substring, replacement);
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine($"The decrypted message is: {message}");
---
+++ FUNDAMEnTALS/FundRetake_2023/01_WorldTour/Program.cs
@@ -0,0 +1,34 @@
+﻿
+string initialStops = Console.ReadLine();
+
+string command = Console.ReadLine();
+
+while (command != "Travel")
+{
+    string[] tokens = command.Split(":", StringSplitOptions.RemoveEmptyEntries);
+
+    if (tokens[0] == "Add Stop")
+    {
+        if (int.Parse(tokens[1]) >= 0 && int.Parse(tokens[1]) < initialStops.Length)
+        {
+            initialStops = initialStops.Insert(int.Parse(tokens[1]), tokens[2]);
+        }
+    }
+    else if (tokens[0] == "Remove Stop")
+    {
+        if (int.Parse(tokens[1]) >= 0 && int.Parse(tokens[1]) < initialStops.Length && int.Parse(tokens[2]) >= 0 && int.Parse(tokens[2]) < initialStops.Length)
+        {
+            initialStops=initialStops.Remove(int.Parse(tokens[1]), int.Parse(tokens[2])+1- int.Parse(tokens[1]));            
+        }
+    }
+    else if(tokens[0] == "Switch")
+    {
+        if (initialStops.Contains(tokens[1]))
+        {
+            initialStops = initialStops.Replace(tokens[1], tokens[2]);
+        }
+    }
+    Console.WriteLine(initialStops);
+    command = Console.ReadLine();
+}
+Console.WriteLine($"Ready for world tour! Planned stops: {initialStops}");

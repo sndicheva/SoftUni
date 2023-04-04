0235901214cd6a59eb1eb2dfae6ab65f3ceb288d
sndicheva <100013953+sndicheva@users.noreply.github.com>
2023-04-04 20:06:27 +0300

    Add files via upload

---
+++ FUNDAMEnTALS/FundRetake_2023/01_PasswordReset/Program.cs
@@ -0,0 +1,49 @@
+﻿string password = Console.ReadLine();
+
+string command = string.Empty;
+while (true)
+{
+    command = Console.ReadLine();
+
+    if (command == "Done")
+    {
+        break;
+    }
+
+    string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string action = tokens[0];
+    switch (action)
+    {
+        case "TakeOdd":
+            string temp = string.Empty;
+            for (int i = 1; i < password.Length; i+=2)
+            {
+                temp += password[i];
+            }
+            password = temp;
+            Console.WriteLine(password);
+            break;
+        case "Cut":
+            int index = int.Parse(tokens[1]);
+            int length = int.Parse(tokens[2]);
+            password = password.Remove(index, length);
+            Console.WriteLine(password);
+            break;
+        case "Substitute":
+            string substring = tokens[1];
+            string replacement = tokens[2];
+            if (password.Contains(substring))
+            {
+                password = password.Replace(substring, replacement);
+                Console.WriteLine(password);
+            }
+            else
+            {
+                Console.WriteLine("Nothing to replace!");
+            }
+            break;
+    }
+}
+
+Console.WriteLine($"Your password is: {password}");

6e4df2de03599241cbf544312c36e52d30eda676
sndicheva <100013953+sndicheva@users.noreply.github.com>
2023-04-05 15:19:11 +0300

    Add files via upload

---
+++ FUNDAMEnTALS/FundRetake_2023/03_ThePianist/Program.cs
@@ -0,0 +1,95 @@
+﻿
+int count = int.Parse(Console.ReadLine());
+
+Dictionary<string, MusicPiece> musicCollection = new Dictionary<string, MusicPiece>();
+
+for (int i = 0; i < count; i++)
+{
+    string[] inputInfo = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries);
+    string musicPiece = inputInfo[0];
+    string composer = inputInfo[1];
+    string key = inputInfo[2];
+    musicCollection[musicPiece] = new MusicPiece(composer, key);
+}
+
+while (true)
+{
+    string[] command = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries);
+    if (command[0] == "Stop")
+    {
+        break;
+    }
+    switch (command[0])
+    {
+        case "Add":
+            Add(musicCollection, command);
+            break;
+        case "Remove":
+            Remove(musicCollection, command);
+            break;
+        case "ChangeKey":
+            ChangeKey(musicCollection, command);
+            break;
+    }
+}
+foreach (var piece in musicCollection)
+{
+    Console.WriteLine($"{piece.Key} -> Composer: {piece.Value.Composer}, Key: {piece.Value.Key}");
+}
+
+
+void Add(Dictionary<string, MusicPiece> musicCollection, string[] command)
+{
+    string musicPiece = command[1];
+    string composer = command[2];
+    string key = command[3];
+    if (!musicCollection.ContainsKey(musicPiece))
+    {
+        musicCollection.Add(musicPiece, new MusicPiece(composer, key));
+        Console.WriteLine($"{musicPiece} by {composer} in {key} added to the collection!");
+    }
+    else
+    {
+        Console.WriteLine($"{musicPiece} is already in the collection!");
+    }
+}
+
+void Remove(Dictionary<string, MusicPiece> musicCollection, string[] command)
+{
+    string musicPiece = command[1];
+    if (!musicCollection.ContainsKey(musicPiece))
+    {        
+        Console.WriteLine($"Invalid operation! {musicPiece} does not exist in the collection.");
+    }
+    else
+    {
+        musicCollection.Remove(musicPiece);
+        Console.WriteLine($"Successfully removed {musicPiece}!");
+    }
+}
+void ChangeKey(Dictionary<string, MusicPiece> musicCollection, string[] command)
+{
+    string musicPiece = command[1];
+    string newKey = command[2];
+    if (!musicCollection.ContainsKey(musicPiece))
+    {
+        Console.WriteLine($"Invalid operation! {musicPiece} does not exist in the collection.");
+    }
+    else
+    {
+        musicCollection[musicPiece].Key = newKey;
+        Console.WriteLine($"Changed the key of {musicPiece} to {newKey}!");
+    }
+}
+public class MusicPiece
+{
+    public MusicPiece(string composer, string key)
+    {
+        Composer = composer;
+        Key = key;
+    }
+
+    public string Composer { get; set; }
+    public string Key { get; set; }
+
+}
---
+++ FUNDAMEnTALS/FundRetake_2023/03_ThePianist_withList/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+int numbers = int.Parse(Console.ReadLine());
+
+Dictionary<string, Pieces> music = new();
+List<string> piecesNames = new List<string>();
+
+for (int i = 0; i < numbers; i++)
+{
+    string[] input = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries);
+    string piece = input[0];
+    string composer = input[1];
+    string key = input[2];
+
+    music[piece] = new Pieces(composer, key);
+    piecesNames.Add(piece);
+}
+
+while (true)
+{
+    string command = Console.ReadLine();
+    if (command == "Stop")
+    {
+        break;
+    }
+
+    string[] tokens = command.Split("|");
+    string piece = tokens[1];
+
+    switch (tokens[0])
+    {
+        case "Add":
+            AddPiece(tokens, music, piece, piecesNames);
+            break;
+        case "Remove":
+            RemovePiece(music, piece, piecesNames);
+            break;
+        case "ChangeKey":
+            ChangeKey(tokens, music, piece);
+            break;
+    }
+
+}
+
+foreach (var piece in piecesNames)
+{
+    Console.WriteLine($"{piece} -> Composer: {music[piece].Composer}, Key: {music[piece].Key}");
+}
+
+
+static void AddPiece(string[] tokens, Dictionary<string, Pieces> music, string piece, List<string> piecesNames)
+{
+    string composer = tokens[2];
+    string key = tokens[3];
+
+    if (music.ContainsKey(piece))
+    {
+        Console.WriteLine($"{piece} is already in the collection!");
+    }
+    else
+    {
+        piecesNames.Add(piece);
+        music[piece] = new Pieces(composer, key);
+        Console.WriteLine($"{piece} by {composer} in {key} added to the collection!");
+    }
+}
+
+static void RemovePiece(Dictionary<string, Pieces> music, string piece, List<string> piecesNames)
+{
+    if (music.ContainsKey(piece))
+    {
+        piecesNames.Remove(piece);
+        music.Remove(piece);
+        Console.WriteLine($"Successfully removed {piece}!");
+    }
+    else
+    {
+        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+    }
+}
+
+static void ChangeKey(string[] tokens, Dictionary<string, Pieces> music, string piece)
+{
+    string key = tokens[2];
+    if (music.ContainsKey(piece))
+    {
+        music[piece].Key = key;
+        Console.WriteLine($"Changed the key of {piece} to {key}!");
+    }
+    else
+    {
+        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+    }
+}
+public class Pieces
+{
+    public Pieces(string composer, string key)
+    {
+        Composer = composer;
+        Key = key;
+    }
+
+    public string Composer { get; set; }
+    public string Key { get; set; }
+}
+

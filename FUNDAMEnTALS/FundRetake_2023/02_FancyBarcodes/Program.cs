e351c23a3639f7beff32d5d21f1252621152c1db
sndicheva <100013953+sndicheva@users.noreply.github.com>
2023-04-05 13:48:56 +0300

    Add files via upload

---
+++ FUNDAMEnTALS/FundRetake_2023/02_FancyBarcodes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"@#+([A-Z][A-Za-z0-9]{4,}[A-Z])@#+";
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string input = Console.ReadLine();
+
+    Match match = Regex.Match(input, pattern);
+    if (match.Success)
+    {
+        string product = match.Groups[1].Value;
+        string productCode = new string(product.Where(Char.IsDigit).ToArray());
+        
+        if (string.IsNullOrEmpty(productCode))
+        {
+            productCode = "00";
+        }
+        Console.WriteLine($"Product group: {productCode}");
+    }
+    else
+    {
+        Console.WriteLine("Invalid barcode");
+    }
+}

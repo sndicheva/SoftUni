594c4ff70306d5b09e81e8b1c5c9040a53879472
sndicheva <100013953+sndicheva@users.noreply.github.com>
2023-04-05 14:03:17 +0300

    Add files via upload

---
+++ FUNDAMEnTALS/FundRetake_2023/02_DestinationMapper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"([=/])(?<destination>[A-Z][A-Za-z]{2,})\1";
+
+string input = Console.ReadLine();
+
+MatchCollection matches = Regex.Matches(input, pattern);
+List<string> destinations = new List<string>();
+int travelPoints = 0;
+
+foreach (Match match in matches)
+{
+    travelPoints += match.Groups["destination"].Value.Length;
+    destinations.Add(match.Groups["destination"].Value);
+}
+
+Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+Console.WriteLine($"Travel Points: {travelPoints}");

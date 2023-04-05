39474783a7b60a0e60b17bd11858d6247d5bdcc9
sndicheva <100013953+sndicheva@users.noreply.github.com>
2023-04-05 11:11:00 +0300

    Add files via upload

---
+++ FUNDAMEnTALS/FundRetake_2023/02_MirrorWords/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"([@#])(?<firstWord>[A-Za-z]{3,})\1\1(?<secondWord>[A-Za-z]{3,})\1";
+
+string input = Console.ReadLine();
+
+MatchCollection matches = Regex.Matches(input, pattern);
+
+List<KeyValuePair<string, string>> mirrorWords = new List<KeyValuePair<string, string>>();
+
+if (matches.Count > 0)
+{
+    Console.WriteLine($"{matches.Count} word pairs found!");
+
+    foreach (Match match in matches)
+    {
+        string firstWord = match.Groups["firstWord"].Value;
+        string secondWord = match.Groups["secondWord"].Value;
+        if (firstWord == new string(secondWord.Reverse().ToArray()))
+        {
+            mirrorWords.Add(new KeyValuePair<string, string>(firstWord, secondWord));
+        }
+    }
+}
+else
+{
+    Console.WriteLine("No word pairs found!");
+}
+
+if (mirrorWords.Count > 0)
+{
+    Console.WriteLine("The mirror words are:");
+    Console.WriteLine(String.Join(", ", mirrorWords.Select(kvp =>$"{ kvp.Key} <=> {kvp.Value}")));
+}
+else
+{
+    Console.WriteLine("No mirror words!");
+}
---
+++ FUNDAMEnTALS/FundRetake_2023/02_MirrorWords_List/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"([@#])(?<firstWord>[A-Za-z]{3,})\1\1(?<secondWord>[A-Za-z]{3,})\1";
+
+string input = Console.ReadLine();
+
+MatchCollection matches = Regex.Matches(input, pattern);
+
+List<string> mirrorWords = new List<string>();
+
+if (matches.Count > 0)
+{
+    Console.WriteLine($"{matches.Count} word pairs found!");
+    foreach (Match match in matches)
+    {
+        string firstWord = match.Groups["firstWord"].Value;
+        string secondWord = match.Groups["secondWord"].Value;
+        string secondWordReversed = new string(secondWord.Reverse().ToArray());
+        if (firstWord == secondWordReversed)
+        {
+            mirrorWords.Add(firstWord + " <=> " + secondWord);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("No word pairs found!");
+}
+
+if (mirrorWords.Count > 0)
+{
+    Console.WriteLine("The mirror words are:");
+    Console.WriteLine(String.Join(", ", mirrorWords));
+}
+else
+{
+    Console.WriteLine("No mirror words!");
+}

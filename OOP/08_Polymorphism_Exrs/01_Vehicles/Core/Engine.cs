b9b8c5fc98bac1f168f09f81222554394366e952
sndicheva <100013953+sndicheva@users.noreply.github.com>
2023-05-05 16:11:45 +0300

    Add files via upload

---
+++ OOP/08_Polymorphism_Exrs/01_Vehicles/Core/Engine.cs
@@ -0,0 +1,83 @@
+﻿using Vehicles.Core.Interfaces;
+using Vehicles.Factories.Interfaces;
+using Vehicles.IO.Interfaces;
+using Vehicles.Models.Interfaces;
+
+namespace Vehicles.Core;
+
+public class Engine : IEngine
+{
+    private IReader reader;
+    private IWriter writer;
+    private IVehicleFactory vehicleFactory;
+
+    private readonly ICollection<IVehicle> vehicles;
+    public Engine(IReader reader, IWriter writer, IVehicleFactory vehicleFactory)
+    {
+        this.reader = reader;
+        this.writer = writer;
+        this.vehicleFactory = vehicleFactory;
+
+        vehicles = new List<IVehicle>();
+    }
+    public void Run()
+    {
+        vehicles.Add(CreateVehicle());
+        vehicles.Add(CreateVehicle());
+
+        int commandsCount = int.Parse(reader.ReadLine());
+
+        for (int i = 0; i < commandsCount; i++)
+        {
+            try
+            {
+                ProcessCommand();
+            }
+            catch(ArgumentException ex)
+            {
+                writer.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        foreach (IVehicle vehicle in vehicles)
+        {
+            writer.WriteLine(vehicle.ToString());
+        }
+    }
+
+
+    private IVehicle CreateVehicle()
+    {
+        string[] tokens = reader.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        return vehicleFactory.Create(tokens[0], double.Parse(tokens[1]), double.Parse(tokens[2]));
+    }
+    private void ProcessCommand()
+    {
+        string[] commandTokens = reader.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        string command = commandTokens[0];
+        string vehicleType = commandTokens[1];
+
+        IVehicle vehicle = vehicles.FirstOrDefault(v => v.GetType().Name == vehicleType);
+
+        if (vehicle == null)
+        {
+            throw new ArgumentException("Invalid vehicle type");
+        }
+
+        if (command == "Drive")
+        {
+            double distance = double.Parse(commandTokens[2]);
+            writer.WriteLine(vehicle.Drive(distance));
+        }
+        else if (command == "Refuel")
+        {
+            double amount = double.Parse(commandTokens[2]);
+            vehicle.Refuel(amount);
+        }
+    }
+}
---
+++ OOP/08_Polymorphism_Exrs/01_Vehicles/Models/Vehicle.cs
@@ -0,0 +1,38 @@
+﻿using Vehicles.Models.Interfaces;
+
+namespace Vehicles.Models
+{
+    public abstract class Vehicle : IVehicle
+    {
+        private double increasedConsumption;
+
+        protected Vehicle(double fuelQuantity, double fuelConsumption, double increasedConsumption)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+            this.increasedConsumption = increasedConsumption;
+        }
+
+        public double FuelQuantity { get; private set; }
+
+        public double FuelConsumption { get; private set; }
+
+        public string Drive(double distance)
+        {
+            double consumption = FuelConsumption + increasedConsumption;
+            if (FuelQuantity < consumption * distance)
+            {
+                throw new ArgumentException($"{this.GetType().Name} needs refueling");
+            }
+
+            FuelQuantity -= distance * consumption;
+
+            return $"{this.GetType().Name} travelled {distance} km";
+        }
+
+        public virtual void Refuel(double amount)
+            => FuelQuantity += amount;
+        public override string ToString()
+            => $"{this.GetType().Name}: {FuelQuantity:f2}";
+    }
+}
